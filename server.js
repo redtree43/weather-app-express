992350267fc59147c94a1f0dffa6588a67cf270a
Richard Gleeson <gleesonj@hotmail.com>
2018-06-21 14:53:11 +1000

    added bodyParser

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true}))
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
     res.render('index');
+})
+
+app.post('/', function (req, res) {
+    res.render('index');
+    console.log(req.body.city)
 })
 
 app.listen(3000, function() {
